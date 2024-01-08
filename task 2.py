24b339593f26e059bec12e6be8ff5056ae56fa6e
tajsadiq <54053150+tajsadiq@users.noreply.github.com>
2024-01-08 11:49:15 +0400

    Update and rename task 2 to task 2.py

--- task 2.py
+++ task 2.py
@@ -1,8 +1,8 @@
 Task 2: My captain Project
 
 list = eval(input('Enter list of numbers: '))
-ans = []
+output = []
 for i in list:
   if i > 0:
     ans.append(i)
-print('Output: ',ans)
+print('Output: ',output)
